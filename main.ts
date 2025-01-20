4e5654faa32bdd6ed870491cf2cfcac761dcda9b
Lillefotgamer <159481531+Lillefotgamer@users.noreply.github.com>
2025-01-20 20:18:13 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    info.changeScoreBy(1)
+})
+game.showLongText("Reseteo", DialogLayout.Bottom)
+info.setScore(blockSettings.readNumber("CookieAmountSetting"))
+forever(function () {
+    blockSettings.writeNumber("CookieAmountSetting", info.score())
+})
